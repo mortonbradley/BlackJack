23f1c8663ae11e7ecc849d752e36496aa8aded0b
mortonbradley <158191097+mortonbradley@users.noreply.github.com>
2024-04-22 15:48:41 +0100

    Update JavaScript.js

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -137,7 +137,7 @@ function startGame() {
     // Display one face-up card for the dealer
     let faceUpCardImg = document.createElement("img");
     let faceUpCard = deck.pop();
-    faceUpCardImg.src = "BlackJack/Images/" + faceUpCard + ".png";
+    faceUpCardImg.src = "./Images/" + faceUpCard + ".png";
     document.getElementById("dealer-cards").append(faceUpCardImg);
 
 
@@ -145,7 +145,7 @@ function startGame() {
     for (let i = 0; i < 2; i++) {
         let cardImg = document.createElement("img");
         let card = deck.pop();
-        cardImg.src = "BlackJack/Images/" + card + ".png";
+        cardImg.src = "./Images/" + card + ".png";
         yourSum += getValue(card);
         yourAceCount += checkAce(card);
         document.getElementById("your-cards").append(cardImg);
@@ -163,7 +163,7 @@ function hit() {
 
     let cardImg = document.createElement("img");
     let card = deck.pop();
-    cardImg.src = "BlackJack/Images/" + card + ".png";
+    cardImg.src = "./Images/" + card + ".png";
     yourSum += getValue(card);
     yourAceCount += checkAce(card);
     document.getElementById("your-cards").append(cardImg);
@@ -176,13 +176,13 @@ function hit() {
 
 function stay() {
     // Reveal the hidden card
-    document.getElementById("hidden").src = "BlackJack/Images/" + hidden + ".png";
+    document.getElementById("hidden").src = "./Images/" + hidden + ".png";
 
     // Dealer hits until their total is at least 17
     while (dealerSum < 17) {
         let cardImg = document.createElement("img");
         let card = deck.pop();
-        cardImg.src = "BlackJack/Images/" + card + ".png";
+        cardImg.src = "./Images/" + card + ".png";
         dealerSum += getValue(card);
         dealerAceCount += checkAce(card);
         document.getElementById("dealer-cards").append(cardImg);
