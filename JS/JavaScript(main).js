a5134229ba579486030b1192f49b62862b60766b
mortonbradley <158191097+mortonbradley@users.noreply.github.com>
2024-04-23 12:01:53 +0100

    Update JavaScript(main).js

--- JS/JavaScript(main).js
+++ JS/JavaScript(main).js
@@ -1,27 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Add event listener for form submission
+    
     document.getElementById("age-form").addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevent the form from submitting normally
+        event.preventDefault();
         
-        // Get the user's date of birth from the input field
+        
         let dobInput = document.getElementById("dob").value;
         let dob = new Date(dobInput);
         
-        // Calculate the user's age
+       
         let age = calculateAge(dob);
         
-        // Check if the user is over 18 years old
+        
         if (age >= 18) {
-            // Redirect to the page for users over 18
+            
             window.location.href="./Betting-BJ.html";
         } else {
-            // Redirect to the page for users under 18
+            
             window.location.href="./Non-betting BJ.html";
         }
     });
 });
 
-// Function to calculate age based on date of birth
+
 function calculateAge(dateOfBirth) {
     let today = new Date();
     let age = today.getFullYear() - dateOfBirth.getFullYear();
